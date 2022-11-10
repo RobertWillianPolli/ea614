dab8115ce6060b6c659a362365c1841f1ec2a2e5
Robert Polli <robert.polli@hotmail.com>
2022-11-09 21:44:02 -0300

    Revisão da questão D

--- pulso-retangular_D.py
+++ pulso-retangular_D.py
@@ -10,6 +10,20 @@ j= complex(0,1)
 w = np.linspace(0,40,100)
 Xjw = []
 
+'''#Gera a onda quadrada x(t)
+
+T = np.linspace(-tal, tal,200)
+x_e = []
+
+plt.plot(T, signal.square(5 * T/2 + (np.pi / 2)))
+
+plt.xlabel("Tempo")
+plt.ylabel("x(t)")
+plt.title("Pulso retangular")'''
+
+
+# Calcula a transformada da onda quadrada
+
 for omega in w:
     x_e = lambda t: signal.square(5 * t / 2 + (np.pi / 2)) * np.exp(-j * omega * t)
     Xjw.append(abs(integrate.quad(x_e, -tal/2, tal/2)[0]))
@@ -20,5 +34,6 @@ plt.setp(stemlines, 'linestyle', 'dotted')
 plt.xlabel("Frequência angular (rad/s)")
 plt.ylabel("|Hc(jw)|")
 plt.title("Filtro de Chebyshev - wc = 10 rad/s, n = 3")
+
 
 plt.show()
