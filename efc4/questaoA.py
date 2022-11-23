8d26cd1e52f6c698924bf9cd66887a67e356b4ec
Robert Polli <robert.polli@hotmail.com>
2022-11-23 20:37:08 -0300

    Added frequencies axis

--- efc4/questaoA.py
+++ efc4/questaoA.py
@@ -5,22 +5,26 @@ import matplotlib.pyplot as plt
 
 """ Rotina que exibe o espectro de magnitude (X(ejw)) de um sinal discreto """
 
-def espectro(y):
+def espectro(y, fs):
     # modulo da transf. de Fourier
     Y = np.abs(np.fft.fft(y))
     # frequencias avaliadas
     w = np.linspace(0, 2 * math.pi, len(Y))
-    
+
+    f = w * fs
+
     # exibe o grafico do espectro
     plt.figure()
-    plt.plot(w, Y / np.max(Y))
+    #plt.plot(w, Y / np.max(Y))
+    plt.plot(f, Y / np.max(Y))
 
     plt.title("Espectro de frequências")
-    plt.xlabel('$\Omega$ [rad]', fontsize=10)
+    #plt.xlabel('$\Omega$ [rad]', fontsize=10)
+    plt.xlabel('Frequência [Hz]', fontsize=10)
     plt.ylabel('|$Y(e^{j\Omega})$|', fontsize=10)
 
     plt.grid(True)
-    plt.xlim((0, 2 * math.pi))
+    #plt.xlim((0, 2 * math.pi))
     
     plt.show()
 
@@ -31,5 +35,5 @@ def espectro(y):
 Fs, y = wavfile.read("creed_overcome.wav")
 
 y=(y[:,0]+y[:,1])/2
-espectro(y)
+espectro(y, Fs)
 
